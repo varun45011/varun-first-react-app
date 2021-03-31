029d98aeb137b5751e3545eaac68065ceec1305e
varun45011 <varunsareen450@gmail.com>
2021-04-01 01:16:03 +0530

    practice_31_3_2021

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-//using Javascript Expression
+import './index.css';
+// Template literals
 
-const fname = "Varun Sareen";
+const fname = "Varun ";
+const lname = "Sareenasldjlksaj";
 
 ReactDom.render(
   <>
-  <h1>My name is {fname}</h1> 
+  <h1 id="first_class">{`My name is ${fname} ${lname}`}</h1> 
   </>,
   document.getElementById('root')
 );
+
+//date and time
+/** 
+const d = new Date().toLocaleDateString();
+const t = new Date().toLocaleTimeString();
+
+ReactDom.render(
+  <>
+  <p>Todays date is {d}</p>
+  <p>Todays Time is {t}</p>
+  </>,
+  document.getElementById("root")
+);**/
+
